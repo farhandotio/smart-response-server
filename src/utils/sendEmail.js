6791a09443997900decda99da1c6e4c0768b6b53
farhandotio <farhansadik0760@gmail.com>
2026-05-03 09:52:12 +0600

    resend update

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -6,7 +6,8 @@ const resendClient = new Resend(config.RESEND_API_KEY);
 export async function sendEmail({ to, subject, html }) {
   try {
     const response = await resendClient.emails.send({
-      from: 'Smart Response <noreply@smarterresponse.xyz>',
+      from: 'Smart Response <hello@smarterresponse.xyz>',
+      replyTo: 'support@smarterresponse.xyz>',
       to,
       subject,
       html,
