7187b0a4b60966db6a4404bfe04f060f422ab952
farhandotio <farhansadik0760@gmail.com>
2026-05-03 09:09:57 +0600

    env

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -11,32 +11,3 @@ export async function sendEmail({ to, subject, html }) {
         html,
     });
 }
-
-// import nodemailer from 'nodemailer';
-
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-
-// export async function sendEmail({ to, subject, html }) {
-//   const mailOptions = {
-//     from: `"Smart Response" <${process.env.EMAIL_USER}>`,
-//     to,
-//     subject,
-//     html,
-//   };
-
-//   try {
-//     const info = await transporter.sendMail(mailOptions);
-//     return { success: true, messageId: info.messageId };
-//   } catch (error) {
-//     console.error('[Mail Error]', error.message);
-//     return { success: false, error: error.message };
-//   }
-// }
